2159b3732955c011ba6bc896b694c0e170dc1ac1
kardokiyani <91535515+kardokiyani@users.noreply.github.com>
2023-04-19 12:59:08 +0200

    Search input added
    Created a search input to the homepage where the user can search for a specific venue. Also styled it in the css file.

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -6,6 +6,7 @@ const API_BASE_URL = "https://api.noroff.dev/api/v1/holidaze/venues";
 
 function GetVenues() {
   const [venues, setVenues] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
   useEffect(() => {
     async function fetchVenues() {
@@ -20,9 +21,22 @@ function GetVenues() {
     fetchVenues();
   }, []);
 
+  const filteredVenues = venues.filter((venue) =>
+    venue.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
+
   return (
     <div>
-      {venues.map((venue) => (
+      <div className="searchContainer">
+        <input
+          className="searchInputStyle"
+          type="text"
+          placeholder="Search venues"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+      </div>
+      {filteredVenues.map((venue) => (
         <div key={venue.id}>
           <h2>{venue.name}</h2>
           <img src={venue.media} alt={venue.name} className="venueImage" />
