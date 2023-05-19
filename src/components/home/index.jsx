d6b93280192a7647d9b35457b7884437135294f4
kardokiyani <91535515+kardokiyani@users.noreply.github.com>
2023-05-19 19:06:28 +0200

    Homepage updated

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
+
 import { Link } from "react-router-dom";
 
 export const API_BASE_URL = "https://api.noroff.dev/api/v1/holidaze/venues";
